b523e87f21dfbece0248e1a4c556830d4a3ce6ae
Fernando Herrera <fernando.herrera85@gmail.com>
2023-12-18 15:11:18 -0500

    Fin de la sección 3

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
+
+import OpenAI from 'openai';
+
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
 
 @Injectable()
 export class GptService {
 
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+
+
   // Solo va a llamar casos de uso
 
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckUseCase({
+    return await orthographyCheckUseCase( this.openai, {
       prompt: orthographyDto.prompt
     });
   }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,19 +1,52 @@
-
+import OpenAI from 'openai';
 
 interface Options {
   prompt: string;
 }
 
 
-
-export const orthographyCheckUseCase = async( options: Options ) => {
+export const orthographyCheckUseCase = async( openai: OpenAI,  options: Options ) => {
 
   const { prompt } = options;
 
 
-  return {
-    prompt: prompt,
-    apikey: process.env.OPENAI_API_KEY,
-  }
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { 
+        role: "system", 
+        content: `
+        Te serán proveídos textos en español con posibles errores ortográficos y gramaticales,
+        Las palabras usadas deben de existir en el diccionario de la Real Academia Española,
+        Debes de responder en formato JSON, 
+        tu tarea es corregirlos y retornar información soluciones, 
+        también debes de dar un porcentaje de acierto por el usuario,
+        
+
+        Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+        Ejemplo de salida:
+        {
+          userScore: number,
+          errors: string[], // ['error -> solución']
+          message: string, //  Usa emojis y texto para felicitar al usuario
+        }
+        
+        
+        `
+      },
+      {
+        role: 'user',
+        content: prompt,
+      }
+  ],
+    model: "gpt-4",
+    temperature: 0.3,
+    max_tokens: 150,
+  });
+
+  // console.log(completion);
+  const jsonResp = JSON.parse(completion.choices[0].message.content);
+
+  return jsonResp;
 
 }
