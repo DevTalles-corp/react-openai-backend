6d555948507ca8a3900d944e6bb45f1ce2d5eb0a
Fernando Herrera <fernando.herrera85@gmail.com>
2023-12-28 14:13:11 -0500

    Path completo en el caso de uso

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -17,32 +17,32 @@ export const imageGenerationUseCase = async (
   const { prompt, originalImage, maskImage } = options;
 
   // Todo: verificar original image
-  if ( !originalImage || !maskImage ) {
+  if (!originalImage || !maskImage) {
     const response = await openai.images.generate({
       prompt: prompt,
       model: 'dall-e-3',
       n: 1,
       size: '1024x1024',
       quality: 'standard',
-      response_format: 'url'
+      response_format: 'url',
     });
-  
+
     // Todo: guardar la imagen en FS.
-    const fileName = await downloadImageAsPng( response.data[0].url );
-    const url = `${ process.env.SERVER_URL }/gpt/image-generation/${ fileName }`
-  
+    const fileName = await downloadImageAsPng(response.data[0].url);
+    const url = `${process.env.SERVER_URL}/gpt/image-generation/${fileName}`;
+
     return {
       url: url,
       openAIUrl: response.data[0].url,
       revised_prompt: response.data[0].revised_prompt,
-    }
+    };
   }
 
   // originalImage=http://localhost:3000/gpt/image-generation/1703770602518.png
   // maskImage=Base64;ASDKJhaskljdasdlfkjhasdkjlHLKJDASKLJdashlkdjAHSKLJDhALSKJD
-  const pngImagePath = await downloadImageAsPng(originalImage);
-  const maskPath = await downloadBase64ImageAsPng( maskImage )
 
+  const pngImagePath = await downloadImageAsPng(originalImage, true );
+  const maskPath = await downloadBase64ImageAsPng(maskImage, true );
 
   const response = await openai.images.edit({
     model: 'dall-e-2',
@@ -51,18 +51,15 @@ export const imageGenerationUseCase = async (
     mask: fs.createReadStream(maskPath),
     n: 1,
     size: '1024x1024',
-    response_format: 'url'
+    response_format: 'url',
   });
 
-  const fileName = await downloadImageAsPng( response.data[0].url );
-  const url = `${ process.env.SERVER_URL }/gpt/image-generation/${ fileName }`
+  const fileName = await downloadImageAsPng(response.data[0].url);
+  const url = `${process.env.SERVER_URL}/gpt/image-generation/${fileName}`;
 
   return {
-    url: url, 
+    url: url,
     openAIUrl: response.data[0].url,
     revised_prompt: response.data[0].revised_prompt,
-  }
-
-
-  
+  };
 };
